46b5bee0ae91b34b2c87959b09927536c0fe9eac
noway <noway@debian>
2017-12-22 19:31:01 +0300

    Particle Interaction Model config part rewrite

---
+++ ParticleInteractionModel.py
@@ -0,0 +1,57 @@
+import sys
+import h5py
+
+class ParticleInteractionModel():
+
+    def __init__( self ):
+        self.noninteracting = self.pic = False
+        
+    @classmethod
+    def init_from_config( cls, conf ):
+        new_obj = cls()
+        new_obj.check_correctness_of_related_config_fields( conf )
+        new_obj.get_values_from_config( conf )
+        return new_obj
+        
+    def check_correctness_of_related_config_fields( self, conf ):
+        conf_part = conf["Particle_interaction_model"]        
+        model = conf_part["particle_interaction_model"]        
+        # 'PIC' or 'noninteracting'
+        if model != "noninteracting" or model != "PIC":
+            print( "Error: wrong value of 'particle_interaction_model': {}".format( model ))
+            print( "Allowed values : 'noninteracting', 'PIC'" )
+            print( "Aborting" )
+            sys.exit( -1 ) 
+
+    def get_values_from_config( self, conf ):
+        conf_part = conf["Particle_interaction_model"]
+        self.particle_interaction_model = conf_part["particle_interaction_model"]        
+        if self.particle_interaction_model == "noninteracting":
+            self.noninteracting = True
+        elif self.particle_interaction_model == "PIC":
+            self.pic = True
+
+            
+    @classmethod
+    def init_from_h5( cls, h5group ):
+        new_obj = cls()
+        new_obj.particle_interaction_model = \
+                h5group.attrs["particle_interaction_model"][0]        
+        if new_obj.particle_interaction_model == "noninteracting":
+            new_obj.noninteracting = True
+        elif new_obj.particle_interaction_model == "PIC":
+            new_obj.pic = True
+        return new_obj
+    
+    def __str__( self ):
+        return "Particle interaction model = {}".format( self.particle_interaction_model )
+    
+    def print( self ):
+        print( "### Particle_interaction_model:" )
+        print( self )
+        
+    def write_to_file( self, h5file ):
+        groupname = "/Particle_interaction_model"
+        h5group = h5file.create_group( groupname )
+        h5group.attrs.create("particle_interaction_model",
+                             self.particle_interaction_model )
