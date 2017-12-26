488f97866c13726dfcc6ae8629ed29811d8b3ad2
noway <noway@debian>
2017-12-26 19:55:38 +0300

    Initial port of particle sources

---
+++ ParticleSourceBox.py
@@ -0,0 +1,131 @@
+from ParticleSource import *
+
+# Box source
+
+class ParticleSourceBox( ParticleSource ):
+
+    def __init__( self ):
+        super().__init__()
+
+        
+    @classmethod
+    def init_from_config( cls, conf, this_source_config_part ):
+        new_obj = cls()
+        new_obj.geometry_type = "box"
+        new_obj.check_correctness_of_related_config_fields(
+            conf, this_source_config_part )
+        new_obj.set_parameters_from_config( this_source_config_part )
+        new_obj.generate_initial_particles()
+        return new_obj
+
+    
+    @classmethod
+    def init_from_h5_source_group( h5_source_group ):
+        new_obj = cls()
+        new_obj.geometry_type = "box"
+        new_obj.read_hdf5_source_parameters( h5_source_group )
+        return new_obj
+
+    
+    def check_correctness_of_related_config_fields( self, conf, this_source_config_part ):
+        self.x_left_ge_zero( conf, this_source_config_part )
+        self.x_left_le_particle_source_x_right( conf, this_source_config_part )
+        self.x_right_le_grid_x_size( conf, this_source_config_part )
+        self.y_bottom_ge_zero( conf, this_source_config_part )
+        self.y_bottom_le_particle_source_y_top( conf, this_source_config_part )
+        self.y_top_le_grid_y_size( conf, this_source_config_part )
+        self.z_near_ge_zero( conf, this_source_config_part )
+        self.z_near_le_particle_source_z_far( conf, this_source_config_part )
+        self.z_far_le_grid_z_size( conf, this_source_config_part )
+
+        
+    def set_parameters_from_config( self, this_source_config_part ):
+        self.xleft = this_source_config_part["box_x_left"]
+        self.xright = this_source_config_part["box_x_right"]
+        self.ytop = this_source_config_part["box_y_top"]
+        self.ybottom = this_source_config_part["box_y_bottom"]
+        self.znear = this_source_config_part["box_z_near"]
+        self.zfar = this_source_config_part["box_z_far"]
+
+
+    def read_hdf5_source_parameters( self, this_source_h5_group ):
+        self.xleft = this_source_h5_group.attrs["box_x_left"][0]
+        self.xright = this_source_h5_group.attrs["box_x_xright"][0]
+        self.ytop = this_source_h5_group.attrs["box_y_top"][0]
+        self.ybottom = this_source_h5_group.attrs["box_y_bottom"][0]
+        self.zfar = this_source_h5_group.attrs["box_z_far"][0]
+        self.znear = this_source_h5_group.attrs["box_z_near"][0]
+
+        
+    def x_left_ge_zero( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_x_left"] >= 0,
+            "box_x_left < 0" )
+
+    
+    def x_left_le_particle_source_x_right( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_x_left"] <= \
+            this_source_config_part["box_x_right"],
+            "box_x_left > box_x_right" )
+
+
+    def x_right_le_grid_x_size( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_x_right"] <= conf["Spatial mesh"]["grid_x_size"],
+            "box_x_right > grid_x_size" )
+
+
+    def y_bottom_ge_zero( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_y_bottom"] >= 0,
+            "box_y_bottom < 0" )
+
+
+    def y_bottom_le_particle_source_y_top( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_y_bottom"] <= \
+            this_source_config_part["box_y_top"],
+            "box_y_bottom > box_y_top" )
+
+
+    def y_top_le_grid_y_size( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_y_top"] <= conf["Spatial mesh"]["grid_y_size"],
+            "box_y_top > grid_y_size" )
+
+
+    def z_near_ge_zero( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_z_near"] >= 0,
+            "box_z_near < 0" )
+
+    def z_near_le_particle_source_z_far( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_z_near"] <= this_source_config_part["box_z_far"],
+            "box_z_near > box_z_far" )
+
+    def z_far_le_grid_z_size( self, conf, this_source_config_part ):
+        production_assert( 
+            this_source_config_part["box_z_far"] <= conf["Spatial mesh"]["grid_z_size"],
+            "box_z_far > grid_z_size" )
+
+
+    def write_hdf5_source_parameters( self, this_source_h5_group ):
+        this_source_h5_group.attrs.create( "box_x_left", self.xleft )
+        this_source_h5_group.attrs.create( "box_x_right", self.xright )
+        this_source_h5_group.attrs.create( "box_y_top", self.ytop )
+        this_source_h5_group.attrs.create( "box_y_bottom", self.ybottom )
+        this_source_h5_group.attrs.create( "box_z_far", self.zfar )
+        this_source_h5_group.attrs.create( "box_z_near", self.znear )
+
+
+    def uniform_position_in_source( self ):
+        return self.uniform_position_in_cube()
+
+    
+    def uniform_position_in_cube( self ):
+        p = Vec3d( self.random_in_range( self.xleft, self.xright, self.rnd_state ), 
+                   self.random_in_range( self.ybottom, self.ytop, self.rnd_state ),
+                   self.random_in_range( self.znear, self.zfar, self.rnd_state ) )
+        return p
---
+++ ParticleSourceCylinder.py
@@ -0,0 +1,204 @@
+from ParticleSource import *
+
+# Cylinder source
+
+class ParticleSourceCylinder( ParticleSource ):
+
+    def __init__( self ):
+        super().__init__()
+
+        
+    @classmethod
+    def init_from_config( cls, conf, this_source_config_part ):
+        new_obj = cls()
+        new_obj.geometry_type = "cylinder"
+        new_obj.check_correctness_of_related_config_fields(
+            conf, this_source_config_part )
+        new_obj.set_parameters_from_config( this_source_config_part )
+        new_obj.generate_initial_particles()
+        return new_obj
+
+    @classmethod
+    def init_from_h5_source_group( h5_source_group ):
+        new_obj = cls()
+        new_obj.geometry_type = "cylinder"
+        new_obj.read_hdf5_source_parameters( h5_source_group )
+        return new_obj
+
+
+    def check_correctness_of_related_config_fields( self, conf, this_source_config_part ):
+        # todo:
+        self.radius_gt_zero( conf, this_source_config_part )
+        self.axis_start_x_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_start_x_plus_rad_le_grid_x_size( conf, this_source_config_part )
+        self.axis_start_y_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_start_y_plus_rad_le_grid_y_size( conf, this_source_config_part )
+        self.axis_start_z_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_start_z_plus_rad_le_grid_z_size( conf, this_source_config_part )
+        self.axis_end_x_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_end_x_plus_rad_le_grid_x_size( conf, this_source_config_part )
+        self.axis_end_y_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_end_y_plus_rad_le_grid_y_size( conf, this_source_config_part )
+        self.axis_end_z_min_rad_ge_zero( conf, this_source_config_part )
+        self.axis_end_z_plus_rad_le_grid_z_size( conf, this_source_config_part )
+
+
+    def set_parameters_from_config( self, this_source_config_part ):
+        self.axis_start_x = this_source_config_part["cylinder_axis_start_x"]
+        self.axis_start_y = this_source_config_part["cylinder_axis_start_y"]
+        self.axis_start_z = this_source_config_part["cylinder_axis_start_z"]
+        self.axis_end_x = this_source_config_part["cylinder_axis_end_x"]
+        self.axis_end_y = this_source_config_part["cylinder_axis_end_y"]
+        self.axis_end_z = this_source_config_part["cylinder_axis_end_z"]
+        self.radius = this_source_config_part["cylinder_radius"]
+
+
+    def read_hdf5_source_parameters( self, this_source_h5_group ):
+        self.axis_start_x = this_source_h5_group.attrs["cylinder_axis_start_x"][0]
+        self.axis_start_y = this_source_h5_group.attrs["cylinder_axis_start_y"][0]
+        self.axis_start_z = this_source_h5_group.attrs["cylinder_axis_start_z"][0]
+        self.axis_end_x = this_source_h5_group.attrs["cylinder_axis_end_x"][0]
+        self.axis_end_y = this_source_h5_group.attrs["cylinder_axis_end_y"][0]
+        self.axis_end_z = this_source_h5_group.attrs["cylinder_axis_end_z"][0]
+        self.radius = this_source_h5_group.attrs["cylinder_radius"][0]
+
+        
+    def radius_gt_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "radius < 0" )
+
+
+    def axis_start_x_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_start_x"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_start_x - cylinder_radius < 0" )
+
+
+    def axis_start_x_plus_rad_le_grid_x_size( self, conf, this_source_config_part ):
+	if this_source_config_part["cylinder_axis_start_x"] + \
+           this_source_config_part["cylinder_radius"] > \
+           conf["Spatial mesh"]["grid_x_size"]:
+            raise ValueError( "cylinder_axis_start_x + cylinder_radius > grid_x_size" )
+
+
+    def axis_start_y_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_start_y"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_start_y - cylinder_radius < 0" )
+        
+
+    def axis_start_y_plus_rad_le_grid_y_size( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_start_y"] + \
+           this_source_config_part["cylinder_radius"] > \
+           conf["Spatial mesh"]["grid_y_size"]:
+            raise ValueError( "cylinder_axis_start_y + cylinder_radius > grid_y_size" )
+
+
+    def axis_start_z_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_start_z"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_start_z - cylinder_radius < 0" )
+
+
+    def axis_start_z_plus_rad_le_grid_z_size( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_start_z"] + \
+           this_source_config_part["cylinder_radius"] > \
+           conf["Spatial mesh"]["grid_z_size"]:
+            raise ValueError( "cylinder_axis_start_z + cylinder_radius > grid_z_size" )
+
+        
+    def axis_end_x_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_x"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_end_x - cylinder_radius < 0" )
+
+
+    def axis_end_x_plus_rad_le_grid_x_size( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_x"] + \
+           this_source_config_part["cylinder_radius"] >\
+           conf["Spatial mesh"]["grid_x_size"]:
+            raise ValueError ("cylinder_axis_end_x + cylinder_radius > grid_x_size" )
+
+
+    def axis_end_y_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_y"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_end_y - cylinder_radius < 0" )
+        
+
+    def axis_end_y_plus_rad_le_grid_y_size( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_y"] + \
+           this_source_config_part["cylinder_radius"] > \
+           conf["Spatial mesh"]["grid_y_size"]:
+            raise ValueError( "cylinder_axis_end_y + cylinder_radius > grid_y_size" )
+
+
+    def axis_end_z_min_rad_ge_zero( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_z"] - \
+           this_source_config_part["cylinder_radius"] < 0:
+            raise ValueError( "cylinder_axis_end_z - cylinder_radius < 0" )
+
+
+    def axis_end_z_plus_rad_le_grid_z_size( self, conf, this_source_config_part ):
+        if this_source_config_part["cylinder_axis_end_z"] + \
+           this_source_config_part["cylinder_radius"] > \
+           conf["Spatial mesh"]["grid_z_size"]:
+            raise ValueError( "cylinder_axis_end_z + cylinder_radius > grid_z_size" )
+
+
+    def write_hdf5_source_parameters( self, this_source_h5_group ):
+        this_source_h5_group.attrs.create( "cylinder_axis_start_x", self.axis_start_x )
+        this_source_h5_group.attrs.create( "cylinder_axis_start_y", self.axis_start_y )
+        this_source_h5_group.attrs.create( "cylinder_axis_start_z", self.axis_start_z )
+        this_source_h5_group.attrs.create( "cylinder_axis_end_x", self.axis_end_x )
+        this_source_h5_group.attrs.create( "cylinder_axis_end_y", self.axis_end_y )
+        this_source_h5_group.attrs.create( "cylinder_axis_end_z", self.axis_end_z )
+        this_source_h5_group.attrs.create( "cylinder_radius", self.radius )
+
+
+    def uniform_position_in_source( self ):
+        return self.uniform_position_in_cylinder()
+
+
+    def uniform_position_in_cylinder( self ):
+        # random point in cylinder along z
+        cyl_axis = Vec3d( ( self.axis_end_x - self.axis_start_x ),
+                          ( self.axis_end_y - self.axis_start_y ),
+                          ( self.axis_end_z - self.axis_start_z ) )
+        cyl_axis_length = cyl_axis.length()
+        r = sqrt( self.random_in_range( 0.0, 1.0, self.rnd_state ) ) * self.radius
+        phi = random_in_range( 0.0, 2.0 * M_PI, self.rnd_state )
+        z = random_in_range( 0.0, cyl_axis_length, self.rnd_state )
+        #
+        x = r * cos( phi )
+        y = r * sin( phi )
+        z = z
+        random_pnt_in_cyl_along_z = Vec3d( x, y, z )
+        # rotate:
+        # see "https://en.wikipedia.org/wiki/Rodrigues'_rotation_formula"
+        # todo: Too complicated. Try rejection sampling.
+        unit_cyl_axis = cyl_axis.normalized()
+        unit_along_z = Vec3d( 0, 0, 1.0 )
+        rotation_axis = unit_along_z.cross_product( unit_cyl_axis )
+        rotation_axis_length = rotation_axis.length()
+        if rotation_axis_length == 0:
+            if copysign( 1.0, unit_cyl_axis.z ) >= 0:
+                random_pnt_in_rotated_cyl = random_pnt_in_cyl_along_z
+            else:
+                random_pnt_in_rotated_cyl = random_pnt_in_cyl_along_z.negate()
+        else:
+            unit_rotation_axis = rotation_axis.normalized()
+            rot_cos = unit_cyl_axis.dot_product( unit_along_z )
+            rot_sin = rotation_axis_length
+            random_pnt_in_rotated_cyl = \
+                random_pnt_in_cyl_along_z.times_scalar( rot_cos ) + \
+                unit_rotation_axis.cross_product( random_pnt_in_cyl_along_z ) * rot_sin + \
+                unit_rotation_axis.times_scalar( \
+                    ( 1 - rot_cos ) * \
+                    unit_rotation_axis.dot_product( random_pnt_in_cyl_along_z ) )        
+        # shift:
+        shifted = random_pnt_in_rotated_cyl.add(
+			       Vec3d( self.axis_start_x,
+                                      self.axis_start_y,
+                                      self.axis_start_z ) )
+        return shifted
