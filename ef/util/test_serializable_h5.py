a579a0622569fec1aaa18874e9bef0941702854d
Ivan Kadochnikov <kadivas@jinr.ru>
2018-06-08 11:35:29 +0300

    create class SerializableH5 to encapsulate hdf5 reading and writing

---
+++ ef/util/test_serializable_h5.py
@@ -0,0 +1,24 @@
+import h5py
+import numpy as np
+
+from ef.util.serializable_h5 import SerializableH5
+
+
+class A(SerializableH5):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+def test_serializable_h5(tmpdir):
+    fname = tmpdir.join("test.h5")
+    a = A(1, 2)
+    with h5py.File(fname, "w") as h5:
+        a.save_h5(h5)
+    with h5py.File(fname, "r") as h5:
+        assert A.load_h5(h5) == a
+    a = A(np.arange(100), A(np.array(((1., 2.), (3., 4.))), 'hello'))
+    with h5py.File(fname, "w") as h5:
+        a.save_h5(h5)
+    with h5py.File(fname, "r") as h5:
+        assert A.load_h5(h5) == a
