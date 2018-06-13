e94a52e840d4d6efaaae97f4d6058bf6de2736ce
Ivan <kadivas@jinr.ru>
2018-06-13 16:53:55 +0300

    Let FieldsSolver use _electric_field for a large performance boost

--- test_main.py
+++ test_main.py
@@ -1,11 +1,9 @@
-from ef.config.components import TimeGridConf, SpatialMeshConf
 from ef.config.efconf import EfConf
 from main import main
 
 
 def test_main(mocker, tmpdir):
     config = tmpdir.join("test_main.conf")
-    EfConf(TimeGridConf(4, 2, 1), SpatialMeshConf((3, 3, 3))).export_to_fname(str(config))
+    EfConf().export_to_fname(str(config))
     mocker.patch("sys.argv", ["main.py", str(config)])
     main()
-    assert True
