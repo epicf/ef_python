a21e11b1a7c567ed75cf02f602fd1d54ded9fb4e
noway <noway@debian>
2018-07-28 17:15:38 +0300

    Add write_hdf5_field_parameters virtual method

--- ExternalField.py
+++ ExternalField.py
@@ -10,16 +10,18 @@ class ExternalField:
         self.name = field_conf_name[field_conf_name.rfind(".") + 1 :]
 
 
-    @classmethod
-    def init_from_h5(cls, h5group):
-        new_obj = cls()
-        new_obj.name = os.path.basename(h5group.name)
-        return new_obj
+    def init_common_fields_from_h5(self, h5group):
+        self.name = os.path.basename(h5group.name)
 
 
     def write_to_file(self, h5_fields_group):
         current_field_group = h5_fields_group.create_group("./" + self.name)
         self.write_hdf5_field_parameters(current_field_group)
+
+
+    def write_hdf5_field_parameters(self, current_field_group):
+        # virtual method
+        raise NotImplementedError()
 
 
     def field_at_particle_position(self, particle, current_time):
