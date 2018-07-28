c7f8597b1057462d5e4d0ef8d2ed76f84bebf129
noway <noway@debian>
2018-07-28 17:13:13 +0300

    PEP8 formatting

--- ExternalField.py
+++ ExternalField.py
@@ -2,25 +2,24 @@ import os
 
 class ExternalField:
 
-    def __init__( self ):
-        pass
+    def __init__(self):
+        self.name = None
+
+
+    def init_common_fields_from_config(self, field_conf, field_conf_name):
+        self.name = field_conf_name[field_conf_name.rfind(".") + 1 :]
+
 
     @classmethod
-    def init_from_config( cls, field_conf, field_conf_name ):
-        new_obj = cls()        
-        new_obj.name = field_conf_name[ field_conf_name.rfind(".") + 1 : ]
-        return new_obj
-
-    @classmethod
-    def init_from_h5( cls, h5group ):
-        new_obj = cls() 
-        new_obj.name = os.path.basename( h5group.name )
+    def init_from_h5(cls, h5group):
+        new_obj = cls()
+        new_obj.name = os.path.basename(h5group.name)
         return new_obj
 
 
-    def write_to_file( self, h5_fields_group ):
-        current_field_group = h5_fields_group.create_group( "./" + self.name )
-        self.write_hdf5_field_parameters( current_field_group )
+    def write_to_file(self, h5_fields_group):
+        current_field_group = h5_fields_group.create_group("./" + self.name)
+        self.write_hdf5_field_parameters(current_field_group)
 
 
     def field_at_particle_position(self, particle, current_time):
