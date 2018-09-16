e963aac79e7cd6ba1631d05dd5912bcf4922b56a
andrew <andrew@dell.domainname>
2018-09-16 12:23:42 +0300

    Merge ExternalFieldMagneticUniform and ExternalFieldElectricUniform

--- ExternalField.py
+++ ExternalField.py
@@ -4,19 +4,27 @@ class ExternalField:
 
     def __init__(self):
         self.name = None
+        self.electric_or_magnetic = None
         self.field_type = None
 
 
-    def init_common_fields_from_config(self, field_conf, field_conf_name):
-        self.name = field_conf_name[field_conf_name.rfind(".") + 1 :]
+    def init_common_parameters_from_config(self, field_conf, field_conf_name):
+        self.name = field_conf_name[field_conf_name.rfind(".") + 1:]
+        self.electric_or_magnetic = field_conf["electric_or_magnetic"]
+        if self.electric_or_magnetic not in ["electric", "magnetic"]:
+            raise ValueError(
+                "ExternalField {} should be 'electric' or 'magnetic'".format(self.name))
 
 
-    def init_common_fields_from_h5(self, h5group):
+    def init_common_parameters_from_h5(self, h5group):
         self.name = os.path.basename(h5group.name)
+        self.electric_or_magnetic = h5group.attrs["electric_or_magnetic"]
 
 
     def write_to_file(self, h5_fields_group):
         current_field_group = h5_fields_group.create_group("./" + self.name)
+        current_field_group.attrs["electric_or_magnetic"] = self.electric_or_magnetic
+        current_field_group.attrs["field_type"] = self.field_type
         self.write_hdf5_field_parameters(current_field_group)
 
 
