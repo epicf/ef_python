020def4a247c9a3c590c54d8e6f10405f11274c2
Ivan <kadivas@jinr.ru>
2018-12-11 15:36:04 +0300

    Format Domain.py

--- Domain.py
+++ Domain.py
@@ -9,10 +9,9 @@ from FieldSolver import FieldSolver
 from ExternalFieldsManager import ExternalFieldsManager
 from ParticleInteractionModel import ParticleInteractionModel
 from ParticleSourcesManager import ParticleSourcesManager
-import physical_constants
 
 
-class Domain():
+class Domain:
 
     def __init__(self):
         self.time_grid = None
@@ -25,7 +24,6 @@ class Domain():
         self.particle_interaction_model = None
         self.output_filename_prefix = None
         self.output_filename_suffix = None
-
 
     @classmethod
     def init_from_config(cls, conf):
@@ -43,18 +41,15 @@ class Domain():
         Domain.check_and_print_unused_conf_sections(conf)
         return new_obj
 
-
     def get_output_filename_prefix_and_suffix(self, conf):
         self.output_filename_prefix = conf["OutputFilename"]["output_filename_prefix"]
         self.output_filename_suffix = conf["OutputFilename"]["output_filename_suffix"]
         Domain.mark_outputfilename_sec_as_used(conf)
 
-
     @staticmethod
     def mark_outputfilename_sec_as_used(conf):
         # For now simply mark sections as 'used' instead of removing them.
         conf["OutputFilename"]["used"] = "True"
-
 
     @staticmethod
     def check_and_print_unused_conf_sections(conf):
@@ -67,7 +62,6 @@ class Domain():
             print("If you don't need these sections, please, comment them explicitly.")
             print("Aborting.")
             sys.exit(-1)
-
 
     @classmethod
     def init_from_h5(cls, h5file, filename_prefix, filename_suffix):
@@ -88,34 +82,29 @@ class Domain():
         new_obj.output_filename_suffix = filename_suffix
         return new_obj
 
-
     def start_pic_simulation(self):
         self.eval_and_write_fields_without_particles()
         self.prepare_recently_generated_particles_for_boris_integration()
         self.write_step_to_save()
         self.run_pic()
 
-
     def continue_pic_simulation(self):
         self.run_pic()
-
 
     def run_pic(self):
         total_time_iterations = self.time_grid.total_nodes - 1
         current_node = self.time_grid.current_node
         for i in range(current_node, total_time_iterations):
             print("Time step from {:d} to {:d} of {:d}".format(
-                i, i+1, total_time_iterations))
+                i, i + 1, total_time_iterations))
             self.advance_one_time_step()
             self.write_step_to_save()
-
 
     def prepare_recently_generated_particles_for_boris_integration(self):
         if self.particle_interaction_model.pic:
             self.eval_charge_density()
             self.eval_potential_and_fields()
         self.shift_new_particles_velocities_half_time_step_back()
-
 
     def advance_one_time_step(self):
         self.push_particles()
@@ -125,17 +114,14 @@ class Domain():
             self.eval_potential_and_fields()
         self.update_time_grid()
 
-
     def eval_charge_density(self):
         self.spat_mesh.clear_old_density_values()
         self.particle_to_mesh_map.weight_particles_charge_to_mesh(
             self.spat_mesh, self.particle_sources)
 
-
     def eval_potential_and_fields(self):
         self.field_solver.eval_potential(self.spat_mesh, self.inner_regions)
         self.field_solver.eval_fields_from_potential(self.spat_mesh)
-
 
     def push_particles(self):
         dt = self.time_grid.time_step_size
@@ -145,7 +131,6 @@ class Domain():
             self.spat_mesh, self.external_fields, self.inner_regions,
             self.particle_to_mesh_map, self.particle_interaction_model)
 
-
     def apply_domain_constrains(self):
         # First generate then remove.
         # This allows for overlap of source and inner region.
@@ -153,11 +138,9 @@ class Domain():
         self.apply_domain_boundary_conditions()
         self.remove_particles_inside_inner_regions()
 
-
-#
-# Push particles
-#
-
+    #
+    # Push particles
+    #
 
     def shift_new_particles_velocities_half_time_step_back(self):
         minus_half_dt = -1.0 * self.time_grid.time_step_size / 2.0
@@ -167,22 +150,19 @@ class Domain():
             self.spat_mesh, self.external_fields, self.inner_regions,
             self.particle_to_mesh_map, self.particle_interaction_model)
 
-
-#
-# Apply domain constrains
-#
+    #
+    # Apply domain constrains
+    #
 
     def apply_domain_boundary_conditions(self):
         for src in self.particle_sources.sources:
             src.particles[:] = [p for p in src.particles if not self.out_of_bound(p)]
 
-
     def remove_particles_inside_inner_regions(self):
         for src in self.particle_sources.sources:
             src.particles[:] = \
-              [p for p in src.particles \
-               if not self.inner_regions.check_if_particle_inside_and_count_charge(p)]
-
+                [p for p in src.particles \
+                 if not self.inner_regions.check_if_particle_inside_and_count_charge(p)]
 
     def out_of_bound(self, particle):
         x = particle.position.x
@@ -195,30 +175,26 @@ class Domain():
               (z >= self.spat_mesh.z_volume_size) or (z <= 0)
         return out
 
-
     def generate_new_particles(self):
         self.particle_sources.generate_each_step()
         self.shift_new_particles_velocities_half_time_step_back()
 
-
-#
-# Update time grid
-#
+    #
+    # Update time grid
+    #
 
     def update_time_grid(self):
         self.time_grid.update_to_next_step()
 
-
-#
-# Write domain to file
-#
+    #
+    # Write domain to file
+    #
 
     def write_step_to_save(self):
         current_step = self.time_grid.current_node
         step_to_save = self.time_grid.node_to_save
         if (current_step % step_to_save) == 0:
             self.write()
-
 
     def write(self):
         file_name_to_write = self.construct_output_filename(
@@ -227,7 +203,7 @@ class Domain():
         h5file = h5py.File(file_name_to_write, mode="w")
         if not h5file:
             print("Error: can't open file " + file_name_to_write + \
-                   "to save results of initial field calculation!")
+                  "to save results of initial field calculation!")
             print("Recheck \'output_filename_prefix\' key in config file.")
             print("Make sure the directory you want to save to exists.")
             print("Writing initial fields to file " + file_name_to_write)
@@ -241,7 +217,6 @@ class Domain():
         self.particle_interaction_model.write_to_file(h5file)
         h5file.close()
 
-
     def construct_output_filename(self, output_filename_prefix,
                                   current_time_step, output_filename_suffix):
         filename = output_filename_prefix + \
@@ -249,21 +224,19 @@ class Domain():
                    output_filename_suffix
         return filename
 
-
-#
-# Free domain
-#
+    #
+    # Free domain
+    #
 
     def free(self):
         print("TODO: free domain.\n")
 
-#
-# Various functions
-#
+    #
+    # Various functions
+    #
 
     def print_particles(self):
         self.particle_sources.print_particles()
-
 
     def eval_and_write_fields_without_particles(self):
         self.spat_mesh.clear_old_density_values()
@@ -274,7 +247,7 @@ class Domain():
         h5file = h5py.File(file_name_to_write, mode="w")
         if not h5file:
             print("Error: can't open file " + file_name_to_write + \
-                   "to save results of initial field calculation!")
+                  "to save results of initial field calculation!")
             print("Recheck \'output_filename_prefix\' key in config file.")
             print("Make sure the directory you want to save to exists.")
             print("Writing initial fields to file " + file_name_to_write)
