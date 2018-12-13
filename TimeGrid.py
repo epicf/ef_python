48aefe2456edbaac8569758995e875d2a2b2ac9b
Ivan <kadivas@jinr.ru>
2018-12-13 17:09:10 +0300

    Replace TimeGrid h5 methods with serializableh5

--- TimeGrid.py
+++ TimeGrid.py
@@ -2,11 +2,11 @@ import logging
 from math import ceil
 
 from ef.config.components import time_grid
-from ef.util.data_class import DataClass
+from ef.util.serializable_h5 import SerializableH5
 
 
-class TimeGrid(DataClass):
-    def __init__(self, total_time, time_step_size, time_save_step):
+class TimeGrid(SerializableH5):
+    def __init__(self, total_time, time_step_size, time_save_step, current_time=0.0, current_node=0):
         if total_time <= 0:
             raise ValueError()
         if time_save_step < time_step_size:
@@ -16,8 +16,8 @@ class TimeGrid(DataClass):
         if time_step_size > total_time:
             raise ValueError()
         self.total_time = float(total_time)
-        self.total_nodes = ceil(self.total_time / time_step_size) + 1
-        self.time_step_size = self.total_time / (self.total_nodes - 1)
+        self._total_nodes = ceil(self.total_time / time_step_size) + 1
+        self.time_step_size = self.total_time / (self._total_nodes - 1)
         if self.time_step_size != time_step_size:
             logging.warning("Reducing time step to {:.3E} from {:.3E} "
                             "to fit a whole number of cells."
@@ -27,9 +27,9 @@ class TimeGrid(DataClass):
             logging.warning("Reducing save time step to {:.3E} from {:.3E} "
                             "to fit a whole number of cells."
                             .format(self.time_save_step, time_save_step))
-        self.node_to_save = int(self.time_save_step / self.time_step_size)  # what?
-        self.current_time = 0.0
-        self.current_node = 0
+        self._node_to_save = int(self.time_save_step / self.time_step_size)
+        self.current_time = current_time
+        self.current_node = current_node
 
     def to_component(self):
         return time_grid.TimeGrid(self.total_time, self.time_save_step, self.time_step_size)
@@ -41,15 +41,7 @@ class TimeGrid(DataClass):
 
     @classmethod
     def init_from_h5(cls, h5group):
-        total_time = h5group.attrs["total_time"]
-        time_step_size = h5group.attrs["time_step_size"]
-        time_save_step = h5group.attrs["time_save_step"]
-        grid = cls(total_time, time_step_size, time_save_step)
-        grid.current_time = h5group.attrs["current_time"]
-        grid.total_nodes = h5group.attrs["total_nodes"]
-        grid.current_node = h5group.attrs["current_node"]
-        grid.node_to_save = h5group.attrs["node_to_save"]
-        return grid
+        return cls.load_h5(h5group)
 
     def update_to_next_step(self):
         self.current_node += 1
@@ -58,10 +50,4 @@ class TimeGrid(DataClass):
     def write_to_file(self, h5file):
         groupname = "/TimeGrid"
         h5group = h5file.create_group(groupname)
-        h5group.attrs.create("total_time", self.total_time)
-        h5group.attrs.create("current_time", self.current_time)
-        h5group.attrs.create("time_step_size", self.time_step_size)
-        h5group.attrs.create("time_save_step", self.time_save_step)
-        h5group.attrs.create("total_nodes", self.total_nodes)
-        h5group.attrs.create("current_node", self.current_node)
-        h5group.attrs.create("node_to_save", self.node_to_save)
+        self.save_h5(h5group)
--- test_time_grid.py
+++ test_time_grid.py
@@ -9,10 +9,10 @@ class TestTimeGrid:
         grid = TimeGrid(100, 1, 10)
         assert str(grid) == ("### TimeGrid:\n"
                              "total_time = 100.0\n"
-                             "total_nodes = 101\n"
+                             "_total_nodes = 101\n"
                              "time_step_size = 1.0\n"
                              "time_save_step = 10.0\n"
-                             "node_to_save = 10\n"
+                             "_node_to_save = 10\n"
                              "current_time = 0.0\n"
                              "current_node = 0")
 
