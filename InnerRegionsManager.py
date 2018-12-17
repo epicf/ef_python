d64267696ac2296315c86dafbb4d9d8dc68bd544
Ivan <kadivas@jinr.ru>
2018-12-17 16:20:44 +0300

    Format InnerRegionsManager

--- InnerRegionsManager.py
+++ InnerRegionsManager.py
@@ -1,15 +1,16 @@
 import sys
+
 from InnerRegionBox import InnerRegionBox
-from InnerRegionSphere import InnerRegionSphere
-from InnerRegionCylinder import InnerRegionCylinder
-from InnerRegionTube import InnerRegionTube
 from InnerRegionConeAlongZ import InnerRegionConeAlongZ
+from InnerRegionCylinder import InnerRegionCylinder
+from InnerRegionSphere import InnerRegionSphere
+from InnerRegionTube import InnerRegionTube
+
 
 class InnerRegionsManager():
 
     def __init__(self):
         self.regions = []
-
 
     @classmethod
     def init_from_config(cls, conf, spat_mesh):
@@ -52,12 +53,10 @@ class InnerRegionsManager():
                 InnerRegionsManager.mark_innerreg_sec_as_used(sec_name, conf)
         return new_obj
 
-
     @staticmethod
     def mark_innerreg_sec_as_used(sec_name, conf):
         # For now simply mark sections as 'used' instead of removing them.
         conf[sec_name]["used"] = "True"
-
 
     @classmethod
     def init_from_h5(cls, h5_reg_group, spat_mesh):
@@ -65,7 +64,6 @@ class InnerRegionsManager():
         for reg_group_name in h5_reg_group.keys():
             new_obj.parse_hdf5_inner_region(h5_reg_group[reg_group_name], spat_mesh)
         return new_obj
-
 
     def parse_hdf5_inner_region(self, this_reg_h5_group, spat_mesh):
         geometry_type = this_reg_h5_group.attrs["geometry_type"]
@@ -89,13 +87,11 @@ class InnerRegionsManager():
                   "Unknown inner-region type. Aborting")
             sys.exit(-1)
 
-
     def write_to_file(self, h5file):
         h5group = h5file.create_group("/InnerRegions")
         h5group.attrs.create("number_of_regions", len(self.regions))
         for reg in self.regions:
             reg.write_to_file(h5group)
-
 
     def check_if_particle_inside(self, p):
         for region in self.regions:
@@ -103,23 +99,19 @@ class InnerRegionsManager():
                 return True
         return False
 
-
     def check_if_particle_inside_and_count_charge(self, p):
         for region in self.regions:
             if region.check_if_particle_inside_and_count_charge(p):
                 return True
         return False
 
-
     def print(self):
         for region in self.regions:
             region.print()
 
-
     def print_inner_nodes(self):
         for region in self.regions:
             region.print_inner_nodes()
-
 
     def print_near_boundary_nodes(self):
         for region in self.regions:
