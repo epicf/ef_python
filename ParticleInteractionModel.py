df12fd8edddef03bdac8aef088792e72e4c01f68
Ivan <kadivas@jinr.ru>
2018-12-18 16:52:59 +0300

    Rewrite ParticleInteractionModel with an enum
    Domain is responsible for creating sections

--- ParticleInteractionModel.py
+++ ParticleInteractionModel.py
@@ -1,20 +1,32 @@
+from enum import Enum, auto
+
 from ef.util.serializable_h5 import SerializableH5
+
+
+class Model(Enum):
+    noninteracting = auto()
+    binary = auto()
+    PIC = auto()
+
+    def __repr__(self):
+        return f"Model.{self.name}"
 
 
 class ParticleInteractionModel(SerializableH5):
     @property
     def noninteracting(self):
-        return self.particle_interaction_model == "noninteracting"
+        return self.particle_interaction_model == Model.noninteracting
 
     @property
     def binary(self):
-        return self.particle_interaction_model == "binary"
+        return self.particle_interaction_model == Model.binary
 
     @property
     def pic(self):
-        return self.particle_interaction_model == "PIC"
+        return self.particle_interaction_model == Model.PIC
 
-    def __init__(self, particle_interaction_model=None):
-        if particle_interaction_model not in ("PIC", 'noninteracting', 'binary'):
-            raise ValueError(f"Unexpected particle interaction model: {particle_interaction_model}")
-        self.particle_interaction_model = particle_interaction_model
+    def __init__(self, particle_interaction_model=Model.PIC):
+        if isinstance(particle_interaction_model, Model):
+            self.particle_interaction_model = particle_interaction_model
+        else:
+            self.particle_interaction_model = Model[particle_interaction_model]
