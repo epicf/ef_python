5488bf91ea94530b93bde98eb4c0bbda241dff1a
Ivan <kadivas@jinr.ru>
2018-12-18 12:26:24 +0300

    Format Particle

--- Particle.py
+++ Particle.py
@@ -1,5 +1,6 @@
 import physical_constants
 
+
 class Particle():
 
     def __init__(self, particle_id, charge, mass, position, momentum):
@@ -10,11 +11,9 @@ class Particle():
         self.momentum = momentum
         self.momentum_is_half_time_step_shifted = False
 
-
     def update_position(self, dt):
         pos_shift = self.momentum.times_scalar(dt / self.mass)
         self.position = self.position.add(pos_shift)
-
 
     def field_at_point(self, point):
         dist = point.sub(self.position)
@@ -23,7 +22,6 @@ class Particle():
             return None
         dist_len_cube = dist_len ** 3
         return dist.times_scalar(self.charge / dist_len_cube)
-
 
     def boris_update_momentum(self, dt, total_el_field, total_mgn_field):
         q_quote = dt * self.charge / self.mass / 2.0
@@ -38,11 +36,9 @@ class Particle():
         u_quote = u.add(tmp.cross_product(s))
         self.momentum = u_quote.add(half_el_force).times_scalar(self.mass)
 
-
     def boris_update_momentum_no_mgn(self, dt, total_el_field):
         dp = total_el_field.times_scalar(self.charge * dt)
         self.momentum = self.momentum.add(dp)
-
 
     def print_long(self):
         print("Particle: ")
@@ -55,10 +51,9 @@ class Particle():
                                                                      self.momentum.y,
                                                                      self.momentum.z))
 
-
     def print_short(self):
         print("id: {} x = {:.2f} y = {:.2f} z = {:.2f} "
-              "px = {:.2f} py = {:.2f} pz = {:.2f}".format(
-                  self.id,
-                  self.position.x, self.position.y, self.position.z,
-                  self.momentum.x, self.momentum.y, self.momentum.z))
+              "px = {:.2f} py = {:.2f} pz = {:.2f}"
+              .format(self.id,
+                      self.position.x, self.position.y, self.position.z,
+                      self.momentum.x, self.momentum.y, self.momentum.z))
