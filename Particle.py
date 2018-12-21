f1135df76a01c010dba9c4bd96f85f8ca1f20990
Ivan <kadivas@jinr.ru>
2018-12-21 09:04:39 +0300

    Test particle momentum integration

--- Particle.py
+++ Particle.py
@@ -24,17 +24,17 @@ class Particle(SerializableH5):
         return dist.times_scalar(self.charge / dist_len_cube)
 
     def boris_update_momentum(self, dt, total_el_field, total_mgn_field):
-        q_quote = dt * self.charge / self.mass / 2.0
-        half_el_force = total_el_field.times_scalar(q_quote)
-        v_current = self.momentum.times_scalar(1.0 / self.mass)
-        u = v_current.add(half_el_force)
+        q_quote = dt * self.charge / self.mass / 2.0  # how easy is it to move?  dt * Q / m /2
+        half_el_force = total_el_field.times_scalar(q_quote)  # half the velocity change caused by electric field in dt
+        v_current = self.momentum.times_scalar(1.0 / self.mass)  # current velocity (at -1/2 dt already)
+        u = v_current.add(half_el_force)  # v_minus
         h = total_mgn_field.times_scalar(
-            q_quote / physical_constants.speed_of_light)
+            q_quote / physical_constants.speed_of_light)  # rotation vector t = qB/m * dt/2
         s = h.times_scalar(
-            2.0 / (1.0 + h.dot_product(h)))
-        tmp = u.add(u.cross_product(h))
-        u_quote = u.add(tmp.cross_product(s))
-        self.momentum = u_quote.add(half_el_force).times_scalar(self.mass)
+            2.0 / (1.0 + h.dot_product(h)))  # rotation vector s = 2t / (1 + t**2)
+        tmp = u.add(u.cross_product(h))  # v_prime is v_minus rotated by t
+        u_quote = u.add(tmp.cross_product(s))  # v_plus = v_minus + v_prime * s
+        self.momentum = u_quote.add(half_el_force).times_scalar(self.mass)  # finally add the other half-velocity
 
     def boris_update_momentum_no_mgn(self, dt, total_el_field):
         dp = total_el_field.times_scalar(self.charge * dt)
--- test_particle.py
+++ test_particle.py
@@ -1,5 +1,6 @@
 import h5py
 
+import physical_constants
 from Particle import Particle
 from Vec3d import Vec3d
 
@@ -22,3 +23,19 @@ class TestParticle:
     def test_field_at_point(self):
         p = Particle(123, -16.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         assert p.field_at_point(Vec3d(2., 0., 1.)) == Vec3d(-4, 0, 0)
+        assert p.field_at_point(Vec3d(0., 0., 1.)) is None # TODO: return zero so that total field does not break?
+
+    def test_update_momentum_no_mgn(self):
+        p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
+        p.boris_update_momentum_no_mgn(0.1, Vec3d(-1.0, 2.0, 3.0))
+        assert p.momentum == Vec3d(1.1, -0.2, 2.7)
+
+    def test_update_momentum(self):
+        p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
+        p.boris_update_momentum(0.1, Vec3d(-1.0, 2.0, 3.0), Vec3d(0, 0, 0))
+        assert p.momentum == Vec3d(1.1, -0.2, 2.7)
+
+        p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
+        p.boris_update_momentum(2, Vec3d(-1.0, 2.0, 3.0), Vec3d(2, 0, 0).times_scalar(
+            physical_constants.speed_of_light))
+        assert p.momentum == Vec3d(3, -2, -5)
