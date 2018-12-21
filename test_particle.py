22d4c9d4a2bc5d56fcc5d21a7197536006669e39
Ivan <kadivas@jinr.ru>
2018-12-21 10:16:24 +0300

    Rewrite Particle
    - position and momentum as 3-element ndarray
    - extract boris update method as a function that can work on 2d arrays

--- test_particle.py
+++ test_particle.py
@@ -1,7 +1,9 @@
 import h5py
+import numpy as np
+from numpy.testing import assert_array_equal
 
 import physical_constants
-from Particle import Particle
+from Particle import Particle, boris_update_momentum
 from Vec3d import Vec3d
 
 
@@ -18,24 +20,40 @@ class TestParticle:
     def test_update_position(self):
         p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         p.update_position(10.0)
-        assert p.position == Vec3d(5., 0., 16.)
+        assert_array_equal(p._position, np.array((5., 0., 16.)))
 
     def test_field_at_point(self):
         p = Particle(123, -16.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         assert p.field_at_point(Vec3d(2., 0., 1.)) == Vec3d(-4, 0, 0)
-        assert p.field_at_point(Vec3d(0., 0., 1.)) is None # TODO: return zero so that total field does not break?
+        assert p.field_at_point(Vec3d(0., 0., 1.)) is None  # TODO: return zero so that total field does not break?
 
     def test_update_momentum_no_mgn(self):
         p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         p.boris_update_momentum_no_mgn(0.1, Vec3d(-1.0, 2.0, 3.0))
-        assert p.momentum == Vec3d(1.1, -0.2, 2.7)
+        assert_array_equal(p.momentum, Vec3d(1.1, -0.2, 2.7))
 
     def test_update_momentum(self):
         p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         p.boris_update_momentum(0.1, Vec3d(-1.0, 2.0, 3.0), Vec3d(0, 0, 0))
-        assert p.momentum == Vec3d(1.1, -0.2, 2.7)
+        assert_array_equal(p.momentum, Vec3d(1.1, -0.2, 2.7))
 
         p = Particle(123, -1.0, 2.0, Vec3d(0., 0., 1.), Vec3d(1., 0., 3.))
         p.boris_update_momentum(2, Vec3d(-1.0, 2.0, 3.0), Vec3d(2, 0, 0).times_scalar(
             physical_constants.speed_of_light))
-        assert p.momentum == Vec3d(3, -2, -5)
+        assert_array_equal(p.momentum, Vec3d(3, -2, -5))
+
+
+def test_update_momentum():
+    assert_array_equal(boris_update_momentum(-1, 2, np.array((1, 0, 3)), 0.1, Vec3d(-1.0, 2.0, 3.0), Vec3d(0, 0, 0)),
+                       np.array((1.1, -0.2, 2.7)))
+    assert_array_equal(
+        boris_update_momentum(-1, 2, np.array((1, 0, 3)), 2, Vec3d(-1.0, 2.0, 3.0), Vec3d(2, 0, 0).times_scalar(
+            physical_constants.speed_of_light)), np.array((3, -2, -5)))
+    assert_array_equal(
+        boris_update_momentum(-1, 2, np.array((1, 0, 3)), 2, np.array((-1.0, 2.0, 3.0)), np.array((2, 0, 0)) *
+                              physical_constants.speed_of_light), np.array((3, -2, -5)))
+    assert_array_equal(
+        boris_update_momentum(charge=-1, mass=2, momentum=np.array([(1, 0, 3)]*10), dt=2,
+                              total_el_field=np.array([(-1.0, 2.0, 3.0)]*10),
+                              total_mgn_field=np.array([(2, 0, 0)]*10) * physical_constants.speed_of_light),
+        np.array([(3, -2, -5)]*10))
