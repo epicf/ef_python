6b0db4a2b44c2e630a7167543e700eb4100839cf
Ivan <kadivas@jinr.ru>
2018-12-24 15:22:34 +0300

    Simplify ExternalFieldsManager total_field methods
    No need to pass a whole Particle object just to get total fields
    All they need is a position

--- ExternalFieldsManager.py
+++ ExternalFieldsManager.py
@@ -8,16 +8,22 @@ class ExternalFieldsManager(SerializableH5):
         self.electric = list(electric)
         self.magnetic = list(magnetic)
 
-    def total_electric_field_at_particle_position(self, particle, current_time):
+    def total_electric_field_at_position(self, position, current_time):
         total_el_field = Vec3d.zero()
         for f in self.electric:
-            el_field = f.field_at_position(Vec3d(*particle._position), current_time)
+            el_field = f.field_at_position(Vec3d(*position), current_time)
             total_el_field = total_el_field.add(el_field)
         return total_el_field
 
-    def total_magnetic_field_at_particle_position(self, particle, current_time):
+    def total_electric_field_at_particle_position(self, particle, current_time):
+        return self.total_electric_field_at_position(particle._position, current_time)
+
+    def total_magnetic_field_at_position(self, position, current_time):
         total_mgn_field = Vec3d.zero()
         for f in self.magnetic:
-            mgn_field = f.field_at_position(Vec3d(*particle._position), current_time)
+            mgn_field = f.field_at_position(Vec3d(*position), current_time)
             total_mgn_field = total_mgn_field.add(mgn_field)
         return total_mgn_field
+
+    def total_magnetic_field_at_particle_position(self, particle, current_time):
+        return self.total_magnetic_field_at_position(particle._position, current_time)
--- test_fields.py
+++ test_fields.py
@@ -3,6 +3,7 @@ from numpy.testing import assert_array_almost_equal
 from ExternalFieldExpression import ExternalFieldExpression
 from ExternalFieldFromFile import ExternalFieldFromFile
 from ExternalFieldUniform import ExternalFieldUniform
+from ExternalFieldsManager import ExternalFieldsManager
 from Vec3d import Vec3d
 
 
@@ -13,12 +14,19 @@ class TestFields:
         assert f.field_at_position(Vec3d(1, 2, 3), 0.) == Vec3d(3.14, 2.7, -0.5)
         assert f.field_at_position(Vec3d(1, 2, 3), 5.) == Vec3d(3.14, 2.7, -0.5)
         assert f.field_at_position(Vec3d(3, 2, 1), 5.) == Vec3d(3.14, 2.7, -0.5)
+        m = ExternalFieldsManager([f], [f, f])
+        assert_array_almost_equal(m.total_electric_field_at_position((0, 0, 0), 2), Vec3d(3.14, 2.7, -0.5))
+        assert_array_almost_equal(m.total_magnetic_field_at_position((1, 2, 3), 5), Vec3d(6.28, 5.4, -1))
 
     def test_expression(self):
         f = ExternalFieldExpression('e1', 'electric', '-1+t', 'x*y-z', 'x+y*z')
         assert f.field_at_position(Vec3d(1, 2, 3), 0.) == Vec3d(-1, -1, 7)
         assert f.field_at_position(Vec3d(1, 2, 3), 5.) == Vec3d(4, -1, 7)
         assert f.field_at_position(Vec3d(3, 2, 1), 5.) == Vec3d(4, 5, 5)
+        m = ExternalFieldsManager([f], [])
+        assert_array_almost_equal(m.total_electric_field_at_position((1, 2, 3), 0), Vec3d(-1, -1, 7))
+        assert_array_almost_equal(m.total_electric_field_at_position((1, 2, 3), 5), Vec3d(4, -1, 7))
+        assert_array_almost_equal(m.total_electric_field_at_position((3, 2, 1), 5), Vec3d(4, 5, 5))
 
     def test_from_file(self):
         f = ExternalFieldFromFile('f1', 'electric', 'examples/test_field.csv')
@@ -28,3 +36,14 @@ class TestFields:
         assert f.field_at_position(Vec3d(.5, .5, .5), 10) == Vec3d(1, 1, 1)
         assert_array_almost_equal(f.field_at_position(Vec3d(.5, 1., .3), 10.), (0., .5, 1.))
         assert_array_almost_equal(f.field_at_position(Vec3d(0, .5, .7), 10), (1, 1.5, 2))
+        m = ExternalFieldsManager([f], [])
+        assert_array_almost_equal(m.total_electric_field_at_position((.5, 1., .3), 10.), Vec3d(0, .5, 1))
+        assert_array_almost_equal(m.total_electric_field_at_position((0, .5, .7), 10), Vec3d(1, 1.5, 2))
+
+    def test_manager(self):
+        u = ExternalFieldUniform('u1', 'electric', (3.14, 2.7, -0.5))
+        e = ExternalFieldExpression('e1', 'electric', '-1+t', 'x*y-z', 'x+y*z')
+        f = ExternalFieldFromFile('f1', 'electric', 'examples/test_field.csv')
+        m = ExternalFieldsManager([u, e, f], [e, u, f])
+        assert_array_almost_equal(m.total_electric_field_at_position((.5, 1., .3), 10.), Vec3d(12.14, 3.4, 1.3))
+        assert_array_almost_equal(m.total_magnetic_field_at_position((.5, 1., .3), 10.), Vec3d(12.14, 3.4, 1.3))
