864dd52d56c09cd43a280fba18f99eb1c61f0c03
Ivan <kadivas@jinr.ru>
2018-12-28 14:10:31 +0300

    Move package code to src/ for simpler setup and testing

--- examples/axially_symmetric_beam_contour/run_example.sh
+++ examples/axially_symmetric_beam_contour/run_example.sh
@@ -1,5 +1,5 @@
 python3 ../../main.py contour.conf
 python3 beam_contour.py
 
-#python3 ../../main.py contour_bin.conf
+#python3 ../../ef_main.py contour_bin.conf
 #python3 beam_contour_bin.py
--- examples/single_particle_in_magnetic_field/run_example.sh
+++ examples/single_particle_in_magnetic_field/run_example.sh
@@ -1,6 +1,6 @@
 python3 ../../main.py single_particle_in_magnetic_field.conf
 python3 plot.py
-#python3 ../../main.py large_time_step.conf
+#python3 ../../ef_main.py large_time_step.conf
 #python3 plot_large_time_step.py
-#python3 ../../main.py long_simulation_time.conf
+#python3 ../../ef_main.py long_simulation_time.conf
 #python3 plot_long_simulation_time.py
