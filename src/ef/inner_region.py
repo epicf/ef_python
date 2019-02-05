5153365833d672b67d686b0907d5bd1152c61521
Ivan <kadivas@jinr.ru>
2019-02-05 13:40:27 +0100

    Allow inner regions to be inverted: only *allow* particles inside region

--- src/ef/inner_region.py
+++ src/ef/inner_region.py
@@ -5,12 +5,13 @@ from ef.util.serializable_h5 import SerializableH5
 
 class InnerRegion(SerializableH5):
 
-    def __init__(self, name, shape, potential=0.0, total_absorbed_particles=0, total_absorbed_charge=0.0):
+    def __init__(self, name, shape, potential=0.0, total_absorbed_particles=0, total_absorbed_charge=0.0, inverted=False):
         self.name = name
         self.shape = shape
         self.potential = potential
         self.total_absorbed_particles = total_absorbed_particles
         self.total_absorbed_charge = total_absorbed_charge
+        self.inverted = inverted
 
     def collide_with_particles(self, particles):
         collisions = self.check_if_points_inside(particles.positions)
@@ -20,4 +21,7 @@ class InnerRegion(SerializableH5):
         particles.remove(collisions)
 
     def check_if_points_inside(self, positions):
-        return self.shape.are_positions_inside(positions)
+        pos_inside = self.shape.are_positions_inside(positions)
+        if self.inverted:
+            pos_inside = np.logical_not(pos_inside)
+        return pos_inside
--- tests/test_inner_region.py
+++ tests/test_inner_region.py
@@ -12,6 +12,7 @@ class TestInnerRegion:
         assert ir.shape == Box()
         assert ir.total_absorbed_particles == 0
         assert ir.total_absorbed_charge == 0
+        assert ir.inverted == False
 
     def test_absorb_charge(self):
         particles = ParticleArray([1], -2.0, 1.0, (0, 0, 0), np.zeros(3))
@@ -38,3 +39,29 @@ class TestInnerRegion:
         assert ir.total_absorbed_particles == 1
         assert ir.total_absorbed_charge == -2
         assert particles == ParticleArray([2], -2.0, 1.0, [(10, 10, 10)], np.zeros((1, 3)))
+
+    def test_absorb_charge_inverted(self):
+        particles = ParticleArray([1], -2.0, 1.0, (0, 0, 0), np.zeros(3))
+        ir = InnerRegion('test', Box(), inverted=True)
+        assert ir.total_absorbed_particles == 0
+        assert ir.total_absorbed_charge == 0
+        ir.collide_with_particles(particles)
+        assert ir.total_absorbed_particles == 0
+        assert ir.total_absorbed_charge == 0
+        assert particles == ParticleArray([1], -2.0, 1.0, np.zeros((1, 3)), np.zeros((1, 3)))
+        particles = ParticleArray([1], -2.0, 1.0, (10, 10, 10), np.zeros(3))
+        ir = InnerRegion('test', Box(), inverted=True)
+        assert ir.total_absorbed_particles == 0
+        assert ir.total_absorbed_charge == 0
+        ir.collide_with_particles(particles)
+        assert ir.total_absorbed_particles == 1
+        assert ir.total_absorbed_charge == -2
+        assert particles == ParticleArray([], -2.0, 1.0, np.zeros((0, 3)), np.zeros((0, 3)))
+        particles = ParticleArray([1, 2], -2.0, 1.0, [(0, 0, 0), (10, 10, 10)], np.zeros((2, 3)))
+        ir = InnerRegion('test', Box(), inverted=True)
+        assert ir.total_absorbed_particles == 0
+        assert ir.total_absorbed_charge == 0
+        ir.collide_with_particles(particles)
+        assert ir.total_absorbed_particles == 1
+        assert ir.total_absorbed_charge == -2
+        assert particles == ParticleArray([1], -2.0, 1.0, [(0, 0, 0)], np.zeros((1, 3)))
